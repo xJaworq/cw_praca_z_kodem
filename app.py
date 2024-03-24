691ad4d84d8c4aab8bdfba9cf89320a64e362408
xJaworq <162886968+xJaworq@users.noreply.github.com>
2024-03-24 13:10:22 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
